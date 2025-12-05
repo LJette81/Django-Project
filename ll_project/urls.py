acb2efe7ec52f6cfc3faa7fd10f75c7f4d0e5613
LJette81 <ljette@mail.umw.edu>
2025-12-05 21:16:53 +0000

    finally?

--- ll_project/urls.py
+++ ll_project/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('learning_logs.urls')),
-    path('', include('todo.urls')),
+    path('todo/', include('todo.urls')),
 ]
